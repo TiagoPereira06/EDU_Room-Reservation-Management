6ee851f9893a02f6607ac252762b45a80973bf52
Tiago Pereira <tiagomrp06@gmail.com>
2020-05-07 12:29:59 +0100

    eval1

--- src/main/java/pt/isel/ls/handler/label/GetLabel.java
+++ src/main/java/pt/isel/ls/handler/label/GetLabel.java
@@ -9,6 +9,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,13 +18,22 @@ import java.util.List;
 public class GetLabel extends LabelHandler {
     @Override
     public ResultInterface execute(CommandRequest commandRequest, Connection connection) throws SQLException {
-        String getLabelsQuery = "SELECT * FROM labels";
+        String getLabelsQuery = "SELECT * FROM roomlabels";
         PreparedStatement statement = connection.prepareStatement(getLabelsQuery);
         ResultSet resultSet = statement.executeQuery();
-        List<List<String>> labelsResult = new LinkedList<>();
+        HashMap<String, List<String>> labelsResult = new HashMap<>();
 
         while (resultSet.next()) {
-            labelsResult.add(new Label(resultSet.getString("name")).parsePropertiesList());
+            String labelName = resultSet.getString("label");
+            String roomName = resultSet.getString("roomname");
+            if(labelsResult.containsKey(labelName)) { //Ja CONTEM A LABEL - ADICIONA À LISTA
+                //labelsResult.add(new Label(resultSet.getString("name")).parsePropertiesList());
+                labelsResult.get(labelName).add(roomName);
+            }else { //ADICIONA A LABEL E ADIOCIONA A SALA La
+                List<String> rooms = new LinkedList<>();
+                rooms.add(roomName);
+                labelsResult.put(labelName, rooms);
+            }
         }
         return new LabelResult(labelsResult, "GET");
     }
--- src/main/java/pt/isel/ls/handler/label/result/LabelResult.java
+++ src/main/java/pt/isel/ls/handler/label/result/LabelResult.java
@@ -1,24 +1,38 @@
 package pt.isel.ls.handler.label.result;
 
-import pt.isel.ls.handler.Result;
+import pt.isel.ls.handler.ResultInterface;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
-public class LabelResult extends Result {
+public class LabelResult implements ResultInterface {
+    public HashMap<String, List<String>> value;
     private final String type;
 
-    public LabelResult(List<List<String>> value, String type) {
-        super(value);
+    public LabelResult(HashMap<String, List<String>> value, String type) {
+        this.value = value;
         this.type = type;
     }
 
     @Override
     public List<String> columns() {
-        return List.of("Name");
+        return List.of("LabelName","RoomName");
+    }
+
+    @Override
+    public List<List<String>> values() {
+        Set<String> labels = value.keySet();
+        for (String l: labels) {
+            List<String> la = new java.util.ArrayList<>(List.of(l));
+            la.addAll(value.get(l));
+        }
+        return new LinkedList<>();
     }
 
     @Override
     public String description() {
-        return type.concat(" Label");
+        return type.concat("Label");
     }
 }
